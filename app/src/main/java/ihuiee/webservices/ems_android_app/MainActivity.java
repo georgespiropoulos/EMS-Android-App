4acc06b165967c78731a1ef96825e45d2abf44e8
Giorgos Spyropoulos <spyrogeo96@gmail.com>
2022-05-14 21:30:38 +0300

    Feature: layouts complete

--- app/src/main/java/ihuiee/webservices/ems_android_app/MainActivity.java
+++ app/src/main/java/ihuiee/webservices/ems_android_app/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.landing_page);
+        setContentView(R.layout.starting_page);
     }
 }
